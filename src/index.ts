3de40f9e015c8a514a4a5cf719d498444168c298
Yuki Ueda <ikuyadeu@yahoo.co.jp>
2020-09-28 13:01:21 +0900

    fix dependencies

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import * as gitminer from './gitminer'
+import { SplitedRules, outputRuleResultTable } from './rules'
+import * as extend from './ruleExtends'
+import * as eslint_manager from './esint_manager'
+import * as util from './util'
+export {gitminer, SplitedRules, outputRuleResultTable, extend, eslint_manager, util}
