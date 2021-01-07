85240687e07238f922dcfc394f18ff2748f85349
Yuki Ueda <ikuyadeu0513@gmail.com>
2021-01-07 15:28:40 +0900

    add eslint-ts in index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as gitminer from './gitminer';
 import { RuleMap, outputRuleResultTable } from './lint-manager/rule-map';
 import { LintManager } from './lint-manager/lint-manager';
 import { ESLintJSManager } from './lint-manager/eslint/esint-js';
+import { ESLintTSManager } from './lint-manager/eslint/esint-ts';
 import { PMDManager } from './lint-manager/pmd/pmd-java8';
 import { PylintManager } from './lint-manager/pylint/pylint';
-export { gitminer, RuleMap, outputRuleResultTable, LintManager, ESLintJSManager as ESLintManager, PMDManager, PylintManager };
+export { gitminer, RuleMap, outputRuleResultTable, LintManager, ESLintJSManager, ESLintTSManager, PMDManager, PylintManager };
