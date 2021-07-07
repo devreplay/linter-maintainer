c7aec5ee43e53221824f4161d67fa6a3e303f600
Yuki Ueda <ikuyadeu0513@gmail.com>
2021-07-07 14:12:07 +0900

    update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 
-import * as gitminer from './gitminer';
 import { RuleMap, outputRuleResultTable } from './lint-manager/rule-map';
 import { LintManager } from './lint-manager/lint-manager';
 import { ESLintJSManager } from './lint-manager/eslint/esint-js';
 import { ESLintTSManager } from './lint-manager/eslint/esint-ts';
 import { PMDManager } from './lint-manager/pmd/pmd-java8';
 import { PylintManager } from './lint-manager/pylint/pylint';
-export { gitminer, RuleMap, outputRuleResultTable, LintManager, ESLintJSManager, ESLintTSManager, PMDManager, PylintManager };
+export { RuleMap, outputRuleResultTable, LintManager, ESLintJSManager, ESLintTSManager, PMDManager, PylintManager };
